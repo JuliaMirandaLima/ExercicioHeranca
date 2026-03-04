858b82f0c3290df5e9e74d459330c53cf21220b6
JuliaMiranda <julia.miranda.192021@gmail.com>
2026-03-04 09:52:39 -0300

    atividade

---
+++ Atividade.java
@@ -0,0 +1,3 @@
+public interface Atividade{
+    public abstract void postar();
+}
--- Macete.java
+++ Macete.java
@@ -1,4 +1,4 @@
-class Macete extends Post {
+class Macete extends Post implements Atividade{
     String frase;
 
     public Macete(String autor, String materia, String frase) {
@@ -9,5 +9,10 @@ class Macete extends Post {
     @Override
     public void exibirConteudo() {
         System.out.println("Dica de ouro para decorar " + materia + ": " + frase);
+    }
+
+    @Override
+    public void postar() {
+        System.out.println("postar no app");
     }
 }
